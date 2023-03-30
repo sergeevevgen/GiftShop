5866a2a4afe52bc0abedfe446b183ed548b65c15
Евгений Сергеев <alexevgenii@mail.ru>
2023-03-31 01:13:53 +0400

    d

--- GiftShopFileImplement/Implements/MessageInfoStorage.cs
+++ GiftShopFileImplement/Implements/MessageInfoStorage.cs
@@ -1,6 +1,7 @@
 ﻿using GiftShopContracts.BindingModels;
 using GiftShopContracts.StoragesContracts;
 using GiftShopContracts.ViewModels;
+using GiftShopFileImplement.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,17 +21,43 @@ namespace GiftShopFileImplement.Implements
 
         public List<MessageInfoViewModel> GetFilteredList(MessageInfoBindingModel model)
         {
-            throw new NotImplementedException();
+            if (model == null)
+            {
+                return null;
+            }
+            return source.Messages.Where(rec => rec.SenderName.Contains(model.FromMailAddress)).Select(CreateModel).ToList();
         }
 
         public List<MessageInfoViewModel> GetFullList()
         {
-            return source.MessageInfos.Select(CreateModel).ToList();
+            return source.Messages.Select(CreateModel).ToList();
         }
 
         public void Insert(MessageInfoBindingModel model)
         {
-            throw new NotImplementedException();
+            source.Messages.Add(CreateModel(model, new MessageInfo { MessageId = model.MessageId}));
+        }
+
+        private MessageInfo CreateModel(MessageInfoBindingModel model, MessageInfo messageInfo)
+        {
+            messageInfo.ClientId = model.ClientId.HasValue ? model.ClientId.Value : null;
+            messageInfo.DateDelivery = model.DateDelivery;
+            messageInfo.SenderName = model.FromMailAddress;
+            messageInfo.Body = model.Body;
+            messageInfo.Subject = model.Subject;
+            return messageInfo;
+        }
+
+        private MessageInfoViewModel CreateModel(MessageInfo messageInfo)
+        {
+            return new MessageInfoViewModel
+            {
+                MessageId = messageInfo.MessageId,
+                SenderName = messageInfo.SenderName,
+                Body = messageInfo.Body,
+                DateDelivery = messageInfo.DateDelivery,
+                Subject = messageInfo.Subject
+            };
         }
     }
 }
